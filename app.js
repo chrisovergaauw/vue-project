f6e44e3a1afa567e1d235554ff2e99aa498f2108
Chris <chris.overgaauw@gmail.com>
2016-11-25 15:25:57 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+new Vue({
+
+  //Target the div with an id of 'events'	
+  el: '#events',
+
+  //register any values or collections that hold data for the app
+  data: {},
+
+  //function will run when app loads
+  read: function() {}, 
+
+  //methods we want to use in our app are registered here
+  methods: {}
+
+});
+
+
