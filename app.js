51d1fcb93031c881661556a4d68d860cacbcf506
Chris Overgaauw <chris.overgaauw@gmail.com>
2016-11-25 15:48:17 +0000

    forced to switch laptops

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 new Vue({
 
-  //Target the div with an id of 'events'	
+  //Target the div with an id of 'events'
   el: '#events',
 
   //register any values or collections that hold data for the app
   data: {},
 
   //function will run when app loads
-  read: function() {}, 
+  read: function() {},
 
   //methods we want to use in our app are registered here
   methods: {}
